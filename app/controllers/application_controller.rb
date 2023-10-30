83f60ae08bf6652022dce17152c2816cc9f98d3a
varun <varun.sharma@mindfiresolutions.com>
2023-10-30 13:11:21 +0530

    update pundit policy for admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  include Pundit
+  include Pundit::Authorization
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
--- app/policies/product_policy.rb
+++ app/policies/product_policy.rb
@@ -6,33 +6,33 @@ class ProductPolicy < ApplicationPolicy
     @product = product
   end
   
-  # class Scope < Scope
-  #   # NOTE: Be explicit about which records you allow access to!
-  #   # def resolve
-  #   #   scope.all
-  #   # end
-  # end
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    # def resolve
+    #   scope.all
+    # end
+  end
   def index?
     user.admin?
   end
 
-  # def new?
-  #   user.admin?
-  # end
+  def new?
+    user.admin?
+  end
 
-  # def create?
-  #   user.admin?
-  # end
+  def create?
+    user.admin?
+  end
 
-  # def edit?
-  #   user.admin?
-  # end
+  def edit?
+    user.admin?
+  end
 
-  # def update?
-  #   user.admin?
-  # end
+  def update?
+    user.admin?
+  end
 
-  # def destroy?
-  #   user.admin?
-  # end
+  def destroy?
+    user.admin?
+  end
 end
